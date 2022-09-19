826a8bb0a9eb51094c01917b29ecef141f978f33
Alexander Alves <vendas77.rj@gmail.com>
2022-09-19 16:04:09 -0300

    Criando os models libs e routes

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 const express = require("express")
-const app = express()
 const consign = require("consign")
-const PORT = 3000
-
-app.set("json spaces", 4)
+const app = express()
 
 consign()
-  .include("routes")
-  .into(app)
-
-
-
-app.listen(PORT, () => console.log(`NTask API - porta ${PORT}`))
+  .include("libs/config.js")
+  .then("db.js")
+  .then("libs/middlewares.js")
+  .then("routes")
+  .then("libs/boot.js")
+  .into(app);
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,10 +1,41 @@
+const { all } = require("bluebird")
+const { rearg } = require("lodash")
+const { nextTick } = require("process")
+
 module.exports = app => {
-  app.get("/tasks", (req, res) => {
-    res.json({
-      tasks: [
-        { title: "Fazer compas" },
-        { title: "Consertar o pc" }
-      ]
+  const Tasks = app.db.models.Tasks 
+
+  app.route("/tasks")
+    .get((req, res) => {
+      Tasks.findAll({}).then(result => res.json(result))
+        .catch(error => res.status(412).json({ msg: error.message }))
     })
-  })
+    .post((req, res) => {
+      Tasks.create(req.body).then(result => res.json(result))
+        .catch(error => res.status(412).json({ msg: error.message }))
+    })
+
+  app.route("/tasks/:id")
+    .get((req, res) => {
+      Tasks.findAll({ where: req.params })
+        .then(result => {
+          if (result) {
+            res.json(result)
+          } else {
+            res.sendStatus(404)
+          }          
+        })
+        .catch(error => res.status(412).json({ msg: error.message }))
+    })
+    .put((req, res) => {
+      Tasks.findOne({ where: req.params })
+        .then(result => res.sendStatus(404))
+        .catch(error => res.status(412).json({ msg: error.message }))
+    })
+    .delete((req, res) => {
+      Tasks.destroy({ where: req.params })
+        .then(result => res.sendStatus(204))
+        .catch(error => res.status(412).json({ msg: error.message }))
+    })
+  
 }
